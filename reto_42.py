d12ab3db60fe0cc216221556fa650b32b8ab2f28
David <odmh2711@gmail.com>
2022-10-22 14:32:53 -0400

    Add reto 42 solution

--- reto_42.py
+++ reto_42.py
@@ -13,42 +13,107 @@
 """
 import random
 import sys
+import re
+
+CELSIUS = "c"
+FAHRENHEIT = "f"
 
 
 def main() -> None:
-    degrees: float = parse_degrees(sys.argv)
-    # TODO
-    # Add logic to play with the comments
+    degrees: str = parse_degrees(sys.argv)
+
+    if not degrees:
+        print(generate_err_msg())
+        return
+    
+    print(calc_degrees(degrees))
 
 
+def calc_degrees(degrees: str) -> str:
+    """
+        Returns the calculated degrees in a string formatted object
 
-def parse_degrees(degrees: str) -> str | None:
-    # TODO
-    # Parse the command line arguments that will come with flags
-    # i.e python reto_42.py 45° to [c|C]|[f|F]
+        degrees [str]
+            The degrees string parsed to calculate
+        
+        Returns:
+            String with the degrees calculated either Fahrenheit or Celsius accordingly
+    """
+    unity_measure: str = degrees.split("°")[-1].lower() # C (Celsius) or F (Fahrenheit)
+    degrees_to_convert: str = int(degrees.split("°")[0]) # Separator
+
+    if unity_measure == CELSIUS:
+        converted_degrees: int = fahrenheit(degrees_to_convert)
+
+        if converted_degrees > 78:
+            return f"{degrees} = {converted_degrees}°F \n{heat_comments()}"
+        elif converted_degrees < 56:
+            return f"{degrees} = {converted_degrees}°F \n{cold_comments()}"
+        
+        return f"{degrees} = {converted_degrees}°F"
+
+    elif unity_measure == FAHRENHEIT:
+        converted_degrees: int = celsius(degrees_to_convert)
+
+        if converted_degrees > 21:
+            return f"{degrees} = {converted_degrees}°C \n{heat_comments()}"
+        elif converted_degrees < 14:
+            return f"{degrees} = {converted_degrees}°C \n{cold_comments()}"
+        
+        return f"{degrees} = {converted_degrees}°C"
 
 
-    if not degrees.endswith("°") or \
-       not degrees[:-1].isdigit():
+def parse_degrees(params: str) -> str | None:
+    """
+        Parse the arguments given by the user via command-line
+
+        params
+            The command-line argv
+        
+        Return
+            Parsed degrees in the format of 000°[C|F] or None if none valid arguments were given
+    """
+    if not len(params) != 2:
+        # Checking that the params given are valid to make calculations
+        _ = re.search("[0-9]+°[C|F]$", params[-1], re.IGNORECASE) # Looking for patterns i.e: 000°[C|F]
+
+        if not _:
+            return None
+        else:
+            return _.string # String form of the re.Match object
+    else:
         return None
-
-    return degrees[:-1] # returning the numeric part
 
 
 def celsius(degrees: str) -> float:
+    """
+        Function that return degrees given to Celsius degrees
+
+        Param
+            String containing the degrees to convert
+    """
     return (degrees - 32) * (5 / 9)
 
 
 def fahrenheit(degrees: str) -> float:
+    """
+        Function that return degrees given to Fahrenheit degrees
+
+        Param
+            String containing the degrees to convert
+    """
     return (degrees * (9 / 5)) + 32
 
 
-def generate_err_msg(text: str) -> str:
+def generate_err_msg() -> str:
+    """
+        Function that generate random error messages
+    """
     ERROR_WORDS = [
         "Oops, you mean apples?",
-        f"{text} Bananas?",
+        "????",
         "Sorry I didn't get it. Could you try again? 😔",
-        f"Beep boop beep?",
+        "Beep boop beep?",
         "Still not getting it 😿",
         "I think I get it, Try adding a '°' sign at the end 🤔💭"
         "Oops, Try adding a '°' sign at the end"
@@ -58,6 +123,9 @@ def generate_err_msg(text: str) -> str:
 
 
 def cold_comments() -> str:
+    """
+        Function that returns random quotes if the degrees are low
+    """
     COLD_WEATHER_COMMENTS = [
         "Sheesh, its freezing here 🥶❄️",
         "Hey if you make chocolate, I don't mind if you bring me a bit 😳🥺",
@@ -73,6 +141,9 @@ def cold_comments() -> str:
 
 
 def heat_comments() -> str:
+    """
+        Function that returns random comments if the degrees are too high
+    """
     HOT_WEATHER_COMMENTS = [
         "Finally summer baby 😎",
         "Hey wanna go to the beach? 🌴☀️",
@@ -82,7 +153,7 @@ def heat_comments() -> str:
         "Lemonade please 🙏",
         "Summer means happy times and good sunshine ☀️😎",
         "If you're not barefoot, then you're overdressed 🥵",
-        "Friends, sun, sun, and sun, that sounds like a summer to me 🤔💭"
+        "Friends, sun... sun, and sun, that sounds like a summer to me 🤔💭"
     ]
 
     return random.choice(HOT_WEATHER_COMMENTS)
